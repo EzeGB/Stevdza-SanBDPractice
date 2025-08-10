4a8ea18fdcc8838a8c506e6f276957dcecc9da39
Zeke <ezequielgerstel777@gmail.com>
2025-08-10 13:02:52 -0400

    List recycler view Adapter for the custom row

---
+++ app/src/main/java/com/example/stevdza_sanbdpractice/data/fragments/list/ListAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.stevdza_sanbdpractice.data.fragments.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.stevdza_sanbdpractice.data.User
+import com.example.stevdza_sanbdpractice.databinding.CustomRowBinding
+
+class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+
+    private var userList = emptyList<User>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MyViewHolder {
+        val binding = CustomRowBinding.inflate(LayoutInflater.from(
+            parent.context),
+            parent,
+            false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(
+        holder: MyViewHolder,
+        position: Int
+    ) {
+        val currentUser = userList[position]
+        holder.binding.idText.text = currentUser.id.toString()
+        holder.binding.firstnameText.text = currentUser.firstName
+        holder.binding.lastnameText.text = currentUser.lastName
+        holder.binding.ageText.text = currentUser.age.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    fun setData(user: List<User>){
+        this.userList = user
+        notifyDataSetChanged()
+    }
+
+    class MyViewHolder (val binding: CustomRowBinding): RecyclerView.ViewHolder(binding.root){
+
+    }
+
+
+}
